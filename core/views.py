132365adf87fc4c813de2572db11db6d26d699e0
harshad <harshadchavan141@gmail.com>
2020-04-16 21:54:05 +0530

    core/models.py - Created the post model which will be serialized core/serializers.py - created a class PostSerializer that will serialize the Post model with the fields mentioned core/admin.py - registered the Post model core/views.py - added class rst_test_view which rcives the get and post request

--- core/views.py
+++ core/views.py
@@ -4,6 +4,8 @@ from django.http import JsonResponse
 #third party imports
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import PostSerializer
+from .models import Post
 
 
 # Create your views here.
@@ -18,8 +20,15 @@ def test_view(request):
 
 class Rest_test_view(APIView):
     def get(self,request,*args,**kwargs):
-        data = {
-            'Name': 'jhon',
-            'age' : 23,
-        }
-        return Response(data)
+        qs = Post.objects.all()
+        serializer = PostSerializer(qs,many = True)
+        return Response(serializer.data)
+
+
+    def post(self,request,*args,**kwargs):
+        serializer = PostSerializer(data= request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
