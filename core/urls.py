96d86e3a29e669c0b47b6fd8743af049970f9e22
harshad <harshadchavan141@gmail.com>
2020-04-17 19:40:09 +0530

    used mixins to make the class simpler

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
-from .views import Rest_test_view
+# from .views import Rest_test_view
+from .views import PostView
 
 
 app_name = 'core'
@@ -9,7 +10,8 @@ urlpatterns = [
     # ex: /cores/test
     
     path('test/', views.test_view, name='test'),
-    path('rest_test/',Rest_test_view.as_view(),name='rest_test')
+    #path('rest_test/',Rest_test_view.as_view(),name='rest_test')
+    path('rest_test/',PostView.as_view(),name='rest_test')
     
 
     
--- core/views.py
+++ core/views.py
@@ -4,9 +4,11 @@ from django.http import JsonResponse
 #third party imports
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+from rest_framework import generics,mixins
 from rest_framework.response import Response
-from .serializers import PostSerializer
 from .models import Post
+from .serializers import PostSerializer
+
 
 
 # Create your views here.
@@ -19,20 +21,34 @@ def test_view(request):
     }
     return JsonResponse(data)
 
-class Rest_test_view(APIView):
 
-    permission_classes = (IsAuthenticated,)
+class PostView(mixins.CreateModelMixin,mixins.ListModelMixin,generics.GenericAPIView):
+        serializer_class = PostSerializer
+        queryset = Post.objects.all()
 
-    def get(self,request,*args,**kwargs):
-        qs = Post.objects.all()
-        serializer = PostSerializer(qs,many = True)
-        return Response(serializer.data)
+        def get(self,request,*args,**kwargs):
+             return self.list(request,*args,**kwargs)
+
+        def post(self,request,*args,**kwargs):
+            return self.create(request,*args,**kwargs)
 
 
-    def post(self,request,*args,**kwargs):
-        serializer = PostSerializer(data= request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
+
+
+# class Rest_test_view(APIView):
+
+#     permissions_classes = (IsAuthenticated,)
+
+#     def get(self,request,*args,**kwargs):
+#         qs = Post.objects.all()
+#         serializer = PostSerializer(qs,many = True)
+#         return Response(serializer.data)
+
+
+#     def post(self,request,*args,**kwargs):
+#         serializer = PostSerializer(data= request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
