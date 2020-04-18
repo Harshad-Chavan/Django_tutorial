47c83b101c2a42937658fdbbc4e51548e40a4fe6
harshad <harshadchavan141@gmail.com>
2020-04-18 20:10:02 +0530

    weather/views.py -- called an api using python request module extracted the information required into a dictionary and passed it to template  mysite/settings.py -- added the ip of server into allowed host  changed launch.jason added ip and port

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def index_view(request):
-    return render(request,'weather/weather.html',{})
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=286217dad16f0101b72bee339b6b1bcf"
+    city = 'Dombivali'
+    response = requests.get(url.format(city)).json()
+    city_weather = {
+
+        'city':response['name'],
+        'temperature':response['main']['temp'],
+        'description':response['weather'][0]['description'],
+        'icon':response['weather'][0]['icon'],
+    }
+
+    context = {'city_weather':city_weather}
+    return render(request,'weather/weather.html',context)
